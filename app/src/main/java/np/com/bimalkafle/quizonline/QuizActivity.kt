13a24e4eb3ef59146ed2efbdb82978f037f68a78
nareshptt <nareshpt253@gmail.com>
2024-03-17 20:21:20 +0530

    Green color Added

--- app/src/main/java/np/com/bimalkafle/quizonline/QuizActivity.kt
+++ app/src/main/java/np/com/bimalkafle/quizonline/QuizActivity.kt
@@ -117,7 +117,7 @@ class QuizActivity : AppCompatActivity(),View.OnClickListener {
             scoreProgressText.text = "$percentage %"
             if(percentage>60){
                 scoreTitle.text = "Congrats! You have passed"
-                scoreTitle.setTextColor(Color.BLUE)
+                scoreTitle.setTextColor(Color.GREEN)
             }else{
                 scoreTitle.text = "Oops! You have failed"
                 scoreTitle.setTextColor(Color.RED)
